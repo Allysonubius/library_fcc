d45c57a0b5d10a104b9233da52dd2d00a0fe33fd
allysomoliveira <allysomoliveira7@gmail.com>
2022-03-08 23:41:23 -0300

    update

--- server.js
+++ server.js
@@ -1,78 +1,73 @@
 'use strict';
 
-const express     = require('express');
-const cors        = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-const mongoose 					= require('mongoose');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+const mongoose = require('mongoose');
 const app = express();
 
 //connection to mongodb
 
-let dbUrl;
-if (process.env.NODE_ENV === "test") {
-  dbUrl = process.env.TEST_DB;
-} else {
-  dbUrl = process.env.DB;
-}
+let dbUrl = process.env.DB;
 
 mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 // mongoose.set('toJSON', { virtuals: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Successfully connected to MONGODB");
+db.once("open", function() {
+    console.log("Successfully connected to MONGODB");
 });
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+    });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+    res.status(404)
+        .type('text')
+        .send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
+app.listen(process.env.PORT, function() {
+    console.log("Listening on port " + process.env.PORT);
+    if (process.env.NODE_ENV) {
+        console.log('Running Tests...');
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (e) {
+                let error = e;
+                console.log('Tests are not valid:');
+                console.log(error);
+            }
+        }, 1500);
+    }
 });
 
 module.exports = app; //for unit/functional testing
